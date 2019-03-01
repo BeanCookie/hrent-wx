2fdf57368249fa2c9f2e05803a4e80dea0f71f70
loveluzhong <loveluzhong@outlook.com>
2019-03-01 17:32:38 +0800

    更新地区选择

--- src/api/api.js
+++ src/api/api.js
@@ -3,14 +3,21 @@ import {
 } from '@/utils/wxRequest'
 
 const apiMall = 'http://localhost:5000'
+const apiMap = 'https://restapi.amap.com/v3/config'
+const key = '8ea44dbee078819bc8ea60e65debad03'
 
 /**
- * 获取发现好商品接口
- * @param  {[type]} params [description]
- * @return {[type]}        [description]
+ * 搜索住房
  */
 const query = (params) => wxRequest(params, apiMall + '/query')
 
+/**
+ * 获取区域
+ */
+const district = (params) => wxRequest(params, apiMap + '/district')
+
 export default {
-  query
+  key,
+  query,
+  district
 }
